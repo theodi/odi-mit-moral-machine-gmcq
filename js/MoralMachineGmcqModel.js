ebd7657be2341ad5b7403582454d06f570962920
David Tarrant <davetaz@theodi.org>
2022-07-27 16:59:19 +0100

    Changing icons and text on completion

--- js/MoralMachineGmcqModel.js
+++ js/MoralMachineGmcqModel.js
@@ -4,4 +4,5 @@ export default class MoralMachineGmcqModel extends McqModel {
 	isCorrect() {
 		return true;
 	}
+	
 }
--- js/MoralMachineGmcqView.js
+++ js/MoralMachineGmcqView.js
@@ -5,6 +5,9 @@ class MoralMachineGmcqView extends McqView {
   onQuestionRendered() {
     this.$('.js-item-label').imageready(() => this.setReadyStatus());
   }
+  onIsCompleteChange(model, isComplete) {
+    this.$('.btn__action').html(model.get('_buttons')["_reset"]["buttonText"]);
+  }
 
 }
 
