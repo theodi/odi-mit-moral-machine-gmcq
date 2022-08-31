4aa6d536830a788e9de7de58318e001bc050afcf
David Tarrant <davetaz@theodi.org>
2022-08-31 14:36:55 +0100

    Minor changes to help with state restoration from XAPI/SCORM

--- js/MoralMachineGmcqView.js
+++ js/MoralMachineGmcqView.js
@@ -4,9 +4,17 @@ class MoralMachineGmcqView extends McqView {
 
   onQuestionRendered() {
     this.$('.js-item-label').imageready(() => this.setReadyStatus());
+    if (this.model.attributes._isComplete) {
+      this.$('.btn__action').html(this.model.get('_buttons')["_reset"]["buttonText"]);
+    }
   }
+
   onIsCompleteChange(model, isComplete) {
     this.$('.btn__action').html(model.get('_buttons')["_reset"]["buttonText"]);
+  }
+
+  onSubmitted() {
+    this.$('.btn__action').html(this.model.get('_buttons')["_reset"]["buttonText"]);
   }
 
 }
